c73aa5d45dd07bce972ac1ae0c44dd248cf00577
JEferson <jnbe@gft.com>
2024-01-08 15:31:19 +0100

    fix

--- js/chatgpt.js
+++ js/chatgpt.js
@@ -1,62 +1,85 @@
   async function fetchResponseAsStream(prompt) {
-    const key = await getKey();
+//    const key = await getKey();
       const apiUrl = 'https://api.openai.com/v1/chat/completions'; // Replace this with the actual API endpoint
       showWaitingModal(true);
       promptPrepareRequest(prompt, "user");
       promptPrepareRequest("", "system");
-      fetch(apiUrl, {
+//      fetch(apiUrl, {
+//        method: 'POST',
+//        headers: {
+//          'Content-Type': 'application/json',
+//          'Authorization': 'Bearer ' + decrypt(atob("bGlsaXlh"), key)
+//        },
+//        body: JSON.stringify({
+//          "model": "gpt-3.5-turbo-16k",
+//          "stream": true,
+//          "messages": globalPrompt
+//        })
+//      })
+      fetch("https://pis2lv2ircq2xvryj5bdkblnfa0rspzc.lambda-url.eu-central-1.on.aws/", {
         method: 'POST',
+
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + decrypt(atob("bGlsaXlh"), key)
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          "model": "gpt-3.5-turbo-16k",
-          "stream": true,
-          "messages": globalPrompt
+          globalPrompt
         })
       })
       .then(response => {
-        // Check if the response is a readable stream
-        if (!response.body) {
-          throw new Error('Readable streams not supported in this browser.');
-        }
-        console.log(globalPrompt);
-        const reader = response.body.getReader();
-        let contentFinalResult = "";
+          const reader = response.body.getReader();
         function readStream() {
+        let contentFinalResult = "";
           return reader.read().then(({ done, value }) => {
+                  // Check if the response is a readable stream
+          //        if (!response.body) {
+          //          throw new Error('Readable streams not supported in this browser.');
+          //        }
+//                  let contentFinalResult = "";
+//
+//          //        let jsonString = new TextDecoder().decode(response).replace(/data:\s*/g, '');
+//                    contentFinalResult = response.body
+//                    promptPrepareRequestStream(contentFinalResult, "system");
+//                   displayResponse();
+//                  closeWaitingModal();
+//                   activateChat(true);
+//
             if (done) {
               console.log('Stream is done.'); // End of the stream
-              promptPrepareRequestStream(contentFinalResult, "system");
-              displayResponse();
               closeWaitingModal();
+              displayResponse();
               activateChat(true);
               return;
             }
-            try {
-              // Process data as it arrives
-              let jsonString = new TextDecoder().decode(value).replace(/data:\s*/g, '');
-              var dataAux = jsonString.split("}]}")
-
-              dataAux.forEach( data=> {
-                  data = data.trim() + "}]}";
-                  if (data.length > "10") {
-                    var parsedContent = JSON.parse(data);
-                    const content = parsedContent.choices[0].delta.content.replace("undefined","");
-                    contentFinalResult += content;
-                  }
-              });
-              promptPrepareRequestStream(contentFinalResult, "system");
-              displayResponse();
-              // Continue reading the stream
-            } catch(error){
-              console.log("");
-            }
+            let jsonString = new TextDecoder().decode(value).replace(/data:\s*/g, '');
+            promptPrepareRequestStream(jsonString, "system");
+            displayResponse();
+//            try {
+//              // Process data as it arrives
+////              let jsonString = new TextDecoder().decode(value).replace(/data:\s*/g, '');
+//
+////              var dataAux = jsonString.split("}]}")
+//               let jsonString = JSON.parse(new TextDecoder().decode(value).replace(/data:\s*/g, ''));
+//               contentFinalResult = jsonString.body
+//
+////              dataAux.forEach( data=> {
+////                  data = data.trim() + "}]}";
+////                  if (data.length > "10") {
+////                    var parsedContent = JSON.parse(data);
+////                    const content = parsedContent.choices[0].delta.content.replace("undefined","");
+////                    contentFinalResult += content;
+////                  }
+////              });
+//              promptPrepareRequestStream(contentFinalResult, "system");
+//              displayResponse();
+//              // Continue reading the stream
+//            } catch(error){
+//              console.log("");
+//            }
             return readStream();
           });
         }
-
+//
         return readStream();
       })
       .catch(error => {
